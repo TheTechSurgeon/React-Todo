c644f5ecf288335783066e053d6643b57f3f740f
strengthofthemountain <strengthofthemountain@gmail.com>
2020-04-06 20:08:40 -0400

    initialiized project sans changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    };
+
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
+  
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
